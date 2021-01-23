1a1dc2c6405296e542263cf91720a362d0f66596
fujiwara <shintaro.fujiwara@gmail.com>
2021-01-23 22:44:47 +0900

    Initial commit

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * File containing the general information page.
+ *
+ * @package     tool_analys
+ * @category    admin
+ * @copyright   2021 Shintaro Fujiwara <shintaro dot fujiwara at gmail dot com>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace  tool_managecourse;
+
+require(__DIR__.'/../../../config.php');
+require_once($CFG->libdir.'/adminlib.php');
+require_once($CFG->libdir.'/moodlelib.php');
+
+if (isguestuser()) {
+    throw new \require_login_exception('Guests are not allowed here.');
+}
+
+// This is a system level page that operates on other contexts.
+require_login();
+
+admin_externalpage_setup('tool_analys');
+
+admin_externalpage_setup('admins');
+if (!is_siteadmin()) {
+    die;
+}
+
+$url = new \moodle_url('/admin/tool/analys/index.php');
+$PAGE->set_url($url);
+$PAGE->set_title(get_string('analys', 'tool_analys'));
+$PAGE->set_heading(get_string('analys', 'tool_analys'));
+
+$returnurl = new \moodle_url('/admin/tool/analys/index.php');
+$renderer = $PAGE->get_renderer('tool_analys');
+
+echo $OUTPUT->header();
+echo "THis is a test";
+echo $OUTPUT->footer();
---
+++ settings.php
@@ -0,0 +1,40 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Plugin administration pages are defined here.
+ *
+ * @package     tool_analys
+ * @category    admin
+ * @copyright   2021 Shintaro Fujiwara <shintaro dot fujiwara at gmail dot com>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+if ($hassiteconfig) {
+    $settings = new admin_settingpage('tool_analys_settings', new lang_string('pluginname', 'tool_analys'));
+
+    $ADMIN->add('tools', $settings);
+
+    $ADMIN->add(
+        'admin',
+        new admin_externalpage(
+            'tool_analys', get_string('analys', 'tool_analys'),
+            new moodle_url('/admin/tool/analys/index.php')
+        )
+    );
+}
